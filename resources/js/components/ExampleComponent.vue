06679b78610bfbdaf436cdf873755638faa650a4
hikariz01 <kittipak01@gmail.com>
2022-07-15 21:09:48 +0700

    Initial commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                        <div class="btn btn-success" @click="shownumber(5)">
+                            Plus 5
+                        </div>
+                        <div class="btn btn-success" @click="shownumber(10)">
+                            Plus 10
+                        </div>
+                        <a href="/create" class="btn btn-warning">
+                            Go to create
+                        </a>
+                        I'm an example component.
+                        {{number}}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        },
+        data() {
+          return {
+              number: 0
+          }
+        },
+        methods: {
+            shownumber: function (howmany) {
+                this.number += howmany
+            }
+        }
+    }
+
+</script>
