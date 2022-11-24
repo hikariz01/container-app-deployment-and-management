b69554c3ecc645566b7d7e6c53e068d46f6657d5
hikariz01 <kittipak01@gmail.com>
2022-11-24 09:25:42 +0700

    add another button to easy discover(?)

--- resources/views/select_cluster/editCluster.blade.php
+++ resources/views/select_cluster/editCluster.blade.php
@@ -101,6 +101,9 @@
                     </div>
                 </div>
             </div>
+            <button role="button" data-bs-toggle="modal" data-bs-target="#monitor" href="#"
+               class="btn btn-outline-warning text-white">
+                (Optional) Monitor your Cluster</button>
             <button type="button" class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#getToken"><i class="fa fa-info-circle" aria-hidden="true"></i> How to get Token&CACert</button>
             <button type="submit" class="btn btn-outline-success" style="width: 10vw"><i class="fa fa-arrow-right" aria-hidden="true"></i> Continue</button>
         </form>
