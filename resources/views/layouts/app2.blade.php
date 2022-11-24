aee94b3aa9374037a8495bee9d3402031d3f3caf
hikariz01 <kittipak01@gmail.com>
2022-11-24 11:04:16 +0700

    Add more description to monitor guide

--- resources/views/layouts/app2.blade.php
+++ resources/views/layouts/app2.blade.php
@@ -503,11 +503,13 @@ login by username <b>admin</b> and password <b>admin</b>
                                 <img class="img-fluid" src="{{ asset('img/grafana_data.png') }}" alt="">
                                 <p class="mt-3">select <b>Prometheus</b> and fill <b>url</b> in HTTP section with your <b>http://&lt;node-ip&gt;:30900</b> and scroll to bottom and press <b>Save & Test</b></p>
                                 <img class="img-fluid" src="{{ asset('img/grafana_data_done.png') }}" alt="">
-                                <p class="mt-3">on left side go to <b>Dashboard</b> and <b>Import</b> then use <b>ID 13332</b> then press <b>load</b></p>
+                                <h3 class="mt-3">Import Dashboard</h3>
+                                <p>on left side go to <b>Dashboard</b> and <b>Import</b> then use <b>ID 13332</b> then press <b>load</b></p>
                                 <img class="img-fluid" src="{{ asset('img/grafana_import_dashboard.png') }}" alt="">
                                 <p class="mt-3">select your data source and press <b>import</b></p>
                                 <img src="{{ asset('img/grafana_13332.png') }}" alt="" class="img-fluid">
-                                <p class="mt-3">access your dashboard by browse dashboard</p>
+                                <h3 class="mt-3">Access your Dashboard</h3>
+                                <p>access your dashboard by browse dashboard</p>
                                 <img class="img-fluid" src="{{ asset('img/grafana_13332_dashboard.png') }}" alt="">
                             </div>
                         </div>
